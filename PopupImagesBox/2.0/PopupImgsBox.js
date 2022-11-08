2d0766417ce89b99bca4a85e7cee5a7d482ef0b2
FONDANT <fondantcn@qq.com>
2022-11-08 15:54:30 +0800

    准备更新2.0

--- PopupImagesBox/2.0/PopupImgsBox.js
+++ PopupImagesBox/2.0/PopupImgsBox.js
@@ -1,20 +1,13 @@
 (function () {
-    /**
-     * @description:addHtmlTag
-     * @param {Element} 添加的元素
-     * @param {dad} 父元素
-     * @param {set} 对象元素
-     * @return 动态添加标签
-     */
-    const addHtmlTag = (Element,dad,set) => { 
-        let create = document.createElement(Element)
+    const addHtmlTag = (Element, dad, set) => {
+        let create = document.createElement(Element);
         for (let key in set) {
-            create.setAttribute(key, set[key])
+            create.setAttribute(key, set[key]);
         }
-        document.querySelector(dad).appendChild(create)
-    }
-    addHtmlTag("link", "html", {
-        rel: "stylesheet",
-        href:"https://at.alicdn.com/t/c/font_3744158_yul2iukvz89.css"
-    })
+        document.querySelector(dad).appendChild(create);
+    };
+    addHtmlTag('link', 'html', {
+        rel: 'stylesheet',
+        href: 'https://at.alicdn.com/t/c/font_3744158_yul2iukvz89.css',
+    });
 })();

fbae89aa99e64489a3ad1b7d3dbb66ecf1b3c999
Fondant <dulciscn.163.com>
2022-12-20 15:20:50 +0800

    更新添加元素模块

--- PopupImagesBox/2.0/PopupImgsBox.js
+++ PopupImagesBox/2.0/PopupImgsBox.js
@@ -1,13 +1,17 @@
 (function () {
-    const addHtmlTag = (Element, dad, set) => {
-        let create = document.createElement(Element);
+    const addHtmlTag = (Element, dad = "html", set = {}, text = '') => {
+        let create = document.createElement(Element)
+        create.innerHTML = text
         for (let key in set) {
-            create.setAttribute(key, set[key]);
+            create.setAttribute(key, set[key])
         }
-        document.querySelector(dad).appendChild(create);
-    };
+        document.querySelector(dad).appendChild(create)
+    }
     addHtmlTag('link', 'html', {
         rel: 'stylesheet',
         href: 'https://at.alicdn.com/t/c/font_3744158_yul2iukvz89.css',
+    });
+    addHtmlTag('script', 'html', {
+        src: 'https://cdn.jsdelivr.net/npm/jquery@3.6.2/dist/jquery.min.js',
     });
 })();
